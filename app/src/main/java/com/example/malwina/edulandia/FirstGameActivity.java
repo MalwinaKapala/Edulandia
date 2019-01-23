23577ee018c1b7bc395ee7a1e2f68b86adbe1ce7
Malwina Kapala <malwina.gadek@gmail.com>
2019-01-23 13:32:16 +0100

    Answer Pictures Tag assigned. Matching question and correct answer by picture TAG.

--- app/src/main/java/com/example/malwina/edulandia/FirstGameActivity.java
+++ app/src/main/java/com/example/malwina/edulandia/FirstGameActivity.java
@@ -62,24 +62,30 @@ public class FirstGameActivity extends AppCompatActivity {
 
         answer1 = findViewById(R.id.answer1);
         answer1.setImageResource(matches.get(0).getMatch2());
+        answer1.setTag(matches.get(0).getMatch2());
         answer1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
+
                 Log.d("answer1 image", "clicked");
-                if (0 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
-                    answer1.setImageResource(matches.get(0).getMatch1());
+                    answer1.setImageResource(matches.get(currentQuestionId).getMatch1());
                 } else Log.d("answer", "Wrong answer");
             }
         });
 
         answer2 = findViewById(R.id.answer2);
         answer2.setImageResource(matches.get(1).getMatch2());
+        answer2.setTag(matches.get(1).getMatch2());
         answer2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
+
                 Log.d("answer2 image", "clicked");
-                if (1 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
                     answer2.setImageResource(matches.get(currentQuestionId).getMatch1());
                 } else Log.d("answer", "Wrong answer");
@@ -88,11 +94,14 @@ public class FirstGameActivity extends AppCompatActivity {
 
         answer3 = findViewById(R.id.answer3);
         answer3.setImageResource(matches.get(2).getMatch2());
+        answer3.setTag(matches.get(2).getMatch2());
         answer3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
+
                 Log.d("answer3 image", "clicked");
-                if (2 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
                     answer3.setImageResource(matches.get(currentQuestionId).getMatch1());
 
@@ -102,11 +111,13 @@ public class FirstGameActivity extends AppCompatActivity {
 
         answer4 = findViewById(R.id.answer4);
         answer4.setImageResource(matches.get(3).getMatch2());
+        answer4.setTag(matches.get(3).getMatch2());
         answer4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
                 Log.d("answer4 image", "clicked");
-                if (3 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
                     answer4.setImageResource(matches.get(currentQuestionId).getMatch1());
                     } else Log.d("answer", "Wrong answer");
@@ -115,11 +126,13 @@ public class FirstGameActivity extends AppCompatActivity {
 
         answer5 = findViewById(R.id.answer5);
         answer5.setImageResource(matches.get(4).getMatch2());
+        answer5.setTag(matches.get(4).getMatch2());
         answer5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
                 Log.d("answer5 image", "clicked");
-                if (4 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
                     answer5.setImageResource(matches.get(currentQuestionId).getMatch1());
                 } else Log.d("answer", "Wrong answer");
@@ -128,11 +141,13 @@ public class FirstGameActivity extends AppCompatActivity {
 
         answer6 = findViewById(R.id.answer6);
         answer6.setImageResource(matches.get(5).getMatch2());
+        answer6.setTag(matches.get(5).getMatch2());
         answer6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
                 Log.d("answer6 image", "clicked");
-                if (5 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
                     answer6.setImageResource(matches.get(currentQuestionId).getMatch1());
                     } else Log.d("answer", "Wrong answer");
@@ -141,11 +156,13 @@ public class FirstGameActivity extends AppCompatActivity {
 
         answer7 = findViewById(R.id.answer7);
         answer7.setImageResource(matches.get(6).getMatch2());
+        answer7.setTag(matches.get(6).getMatch2());
         answer7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
                 Log.d("answer7 image", "clicked");
-                if (6 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
                     answer7.setImageResource(matches.get(currentQuestionId).getMatch1());
                     } else Log.d("answer", "Wrong answer");
@@ -154,11 +171,13 @@ public class FirstGameActivity extends AppCompatActivity {
 
         answer8 = findViewById(R.id.answer8);
         answer8.setImageResource(matches.get(7).getMatch2());
+        answer8.setTag(matches.get(6).getMatch2());
         answer8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
                 Log.d("answer8 image", "clicked");
-                if (7 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
                     answer8.setImageResource(matches.get(currentQuestionId).getMatch1());
                 } else Log.d("answer", "Wrong answer");
@@ -167,11 +186,13 @@ public class FirstGameActivity extends AppCompatActivity {
 
         answer9 = findViewById(R.id.answer9);
         answer9.setImageResource(matches.get(8).getMatch2());
+        answer9.setTag(matches.get(8).getMatch2());
         answer9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ImageView clickedImage = (ImageView) v;
                 Log.d("answer9 image", "clicked");
-                if (8 == currentQuestionId) {
+                if (clickedImage.getTag().equals(matches.get(currentQuestionId).getMatch2())) {
                     Log.d("answer","Good answer");
                     answer9.setImageResource(matches.get(currentQuestionId).getMatch1());
                 } else Log.d("answer", "Wrong answer");
